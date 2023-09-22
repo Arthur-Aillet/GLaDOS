3cd035b72544bcf20c106c727203f00bc79d9f19
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-09-22 08:51:43 +0200

    feat exo 4 AST part 2

--- part1/app/Main.hs
+++ part1/app/Main.hs
@@ -4,16 +4,11 @@ module Main (main) where
 
 -- Exo 1
 
-data SExpr  = SInt Int
-            | SSym String
-            | SList [SExpr]
-            deriving (Show, Eq)
-
-
--- SList [SSym "define", SSym "x", SInt 5]
--- SSym "x"
--- SList [SSym "if", SList [SSym ">", SSym "x", SInt 4], SInt 1, SInt 0]
--- SList [SSym "define", SSym "y", SList [SSym "+", SInt 5, SSym "x"]]
+data SExpr
+  = SInt Int
+  | SSym String
+  | SList [SExpr]
+  deriving (Show, Eq)
 
 -- Exo 2
 
@@ -34,49 +29,73 @@ getList _ = Nothing
 printInsideList :: [SExpr] -> Maybe String
 printInsideList [] = Nothing
 printInsideList [x] = printTree x
-printInsideList (x:xs) = case (printTree x, printInsideList xs) of
-                        (Just strX, Just strXS) -> Just (strX ++ "," ++ strXS)
-                        _ -> Nothing
-
+printInsideList (x : xs) = case (printTree x, printInsideList xs) of
+  (Just strX, Just strXS) -> Just (strX ++ "," ++ strXS)
+  _ -> Nothing
 
 printTree :: SExpr -> Maybe String
 printTree (SSym s) = Just ("a Symbol " ++ s)
 printTree (SInt i) = Just ("a Number " ++ show i)
-printTree (SList l) = fmap ("a List with "  ++) (printInsideList l)
-
+printTree (SList l) = fmap ("a List with " ++) (printInsideList l)
 
 -- Abstract Syntax Tree
 
 -- Exo 1
 
-data Ast = Define { name :: String, val :: Ast }
-         | AInt { intValue :: Int }
-         | ASym { symbolName :: String }
-         | ABool { boolValue :: Bool }
-         deriving Show
+data Ast
+  = Define {name :: String, val :: Ast}
+  | Call {name :: String, x :: Ast, y :: Ast}
+  | AInt {intValue :: Int}
+  | ASym {symbolName :: String}
+  | ABool {boolValue :: Bool}
+  deriving (Show)
 
 -- Exo 2
 
 sexprToAST :: SExpr -> Maybe Ast
+sexprToAST (SList [SSym "define", SSym var, valExpr]) =
+  case sexprToAST valExpr of
+    Just exprVal -> Just $ Define var exprVal
+    _ -> Nothing
+sexprToAST (SList [SSym name, x, y]) =
+  case (sexprToAST x, sexprToAST y) of
+    (Just x, Just y) -> Just $ Call name x y
+    _ -> Nothing
 sexprToAST (SInt s) = Just $ AInt s
 sexprToAST (SSym s) = Just $ ASym s
-sexprToAST (SList [SSym "define", SSym var, valExpr]) =
-    case sexprToAST valExpr of
-        Just exprVal -> Just $ Define var exprVal
-        _            -> Nothing
 sexprToAST _ = Nothing
+
+data Function a = Operator (a -> a -> a) | OtherSymbol String
+
+instance (Show a) => Show (Function a) where
+    show (OtherSymbol x) = "OtherSymbol " ++ x
+    show (Operator func) = "() () -> ()"
+
+symbolToOperator :: String -> Function Int
+symbolToOperator "+" = Operator (+)
+symbolToOperator "-" = Operator (-)
+symbolToOperator "*" = Operator (*)
+symbolToOperator x = OtherSymbol x
+
+evalAST :: Ast -> Maybe Ast
+evalAST (Call name x y) = case symbolToOperator name of
+    (Operator f) -> case (evalAST x, evalAST y) of
+        (Just (AInt xx), Just (AInt yy)) -> Just $ AInt $ f xx yy
+        _ -> Nothing
+    _ -> Nothing
+evalAST x = Just x
 
 main :: IO ()
 main = do
-    let expr1 = SSym "define"
-        expr2 = SInt 5
-        expr3 = SSym "x"
-        expr5 = SSym "+"
-        expr6 = SSym "y"
-        expr7 = SList [expr5, expr2, expr3]
-        expr4 = SList [expr1, expr6, expr2]
-    putStrLn $ "Symbol 1: " ++ show (getSymbol expr1)
-    putStrLn $ "Symbol 2: " ++ show (getInteger expr2)
-    putStrLn $ "Symbol 3: " ++ show (getList expr3)
-    putStrLn $ "Exo 3:" ++ show (printTree expr4)
-    putStrLn $ "Exo 3 AST:" ++ show (sexprToAST expr4)
+  let expr1 = SSym "define"
+      expr2 = SInt 5
+      expr3 = SSym "x"
+      expr5 = SSym "+"
+      expr6 = SSym "y"
+      expr7 = SList [expr5, expr2, expr3]
+      expr4 = SList [expr1, expr6, expr2]
+  putStrLn $ "Symbol 1: " ++ show (getSymbol expr1)
+  putStrLn $ "Symbol 2: " ++ show (getInteger expr2)
+  putStrLn $ "Symbol 3: " ++ show (getList expr3)
+  putStrLn $ "Exo 3:" ++ show (printTree expr4)
+  putStrLn $ "Exo 3 AST:" ++ show (sexprToAST expr4)
