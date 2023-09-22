244de6c7c7fca7de1aff9aab5abe32b5eab0862a
syborg64 <julian.scott@epitech.eu>
2023-09-22 14:15:11 +0200

    chore : cleanup SPrarser for ETA, readability and library reuse

--- src/SParser.hs
+++ src/SParser.hs
@@ -5,9 +5,14 @@
 -- parser
 --
 
+module SParser where
+
 import Text.Read (readMaybe)
+import GHC.Utils.Misc (split)
 
 data SExpr  = SInt Int
+            | SBool Bool
+            | SFloat Float
             | SSym String
             | SList [SExpr]
             deriving (Show)
@@ -18,25 +23,36 @@ sExprParser str = strArrayToSExpr (sexprSplit str)
 strArrayToSExpr :: [String] -> [SExpr]
 strArrayToSExpr [] = []
 strArrayToSExpr ("(" : xs) = (SList (strArrayToSExpr (getInsideParentheses(("(" : xs)))) : strArrayToSExpr (removeParenthesis ("(" : xs) 0))
-strArrayToSExpr (x : xs) = (strToSExpr x : strArrayToSExpr xs )
+-- TODO: split this function with where statements for legibility
+strArrayToSExpr (x : xs) = (readSExpr x : strArrayToSExpr xs )
 
-strToSExpr :: String -> SExpr
-strToSExpr str = case readMaybe str :: Maybe Int of
-                    Just i -> SInt i
+
+readBool :: String -> Maybe Bool
+readBool "#f" = Just False
+readBool "#t" = Just True
+readBool _ = Nothing
+
+readSExpr :: String -> SExpr
+readSExpr str = case (readMaybe str :: Maybe Int, readMaybe str :: Maybe Float, readBool str) of
+                    (_, _, Just b) -> SBool b
+                    (Just i, _, _) -> SInt i
+                    (_, Just f, _) -> SFloat f
                     _ -> SSym str
 
 sexprSplit :: String -> [String]
-sexprSplit input = cleanStrings (cleanStrings (separateCharOnList (separateCharOnList (strSplit input ' ') '(') ')') "") "\n"
+sexprSplit input = clean (clean (separateCharOnList (separateCharOnList (split ' ' input) '(') ')') "") "\n"
+-- TODO: split this function with where statements for legibility
 
-isBetween :: Char -> Char -> Char -> Bool
-isBetween c smaller bigger  | c > bigger = False
-                            | c < smaller = False
-                            | otherwise = True
+isBetween :: (Ord a, Eq a) => a -> a -> a -> Bool
+isBetween a b c = (a >= b) && (a <= c )
+-- isBetween c smaller bigger  | c > bigger = False
+--                             | c < smaller = False
+--                             | otherwise = True
 
-strSplit :: String -> Char -> [String]
-strSplit [] _ = [""]
-strSplit (x : xs) c | x == c = ("" : strSplit xs c)
-                    | otherwise = (x : head (strSplit xs c)) : tail (strSplit xs c)
+-- strSplit :: String -> Char -> [String]
+-- strSplit [] _ = [""]
+-- strSplit (x : xs) c | x == c = ("" : strSplit xs c)
+--                     | otherwise = (x : head (strSplit xs c)) : tail (strSplit xs c)
 
 separateChar :: String -> Char -> [String]
 separateChar [] _ = [""]
@@ -47,10 +63,13 @@ separateCharOnList :: [String] -> Char -> [String]
 separateCharOnList [] _ = []
 separateCharOnList (x : xs) c = separateChar x c ++ separateCharOnList xs c
 
-cleanStrings :: [String] -> String -> [String]
-cleanStrings [] _ = []
-cleanStrings (x : xs) str   | x == str = cleanStrings xs str
-                            | otherwise = (x : cleanStrings xs str)
+clean :: (Eq a) => [a] -> a -> [a]
+clean xs match = filter (match /= ) xs
+
+-- cleanStrings :: [String] -> String -> [String]
+-- cleanStrings [] _ = []
+-- cleanStrings (x : xs) str   | x == str = cleanStrings xs str
+--                             | otherwise = (x : cleanStrings xs str)
 
 getParenthesis :: [String] -> Int -> [String]
 getParenthesis [] _ = []
@@ -66,13 +85,12 @@ removeParenthesis (x : xs) i   | x == "(" = (removeParenthesis xs (i + 1))
                             | x == ")" = (removeParenthesis xs (i - 1))
                             | otherwise = (removeParenthesis xs i)
 
-removeLast :: [String] -> [String]
-removeLast (x : y : []) = (x : [])
-removeLast (x : xs) = (x : removeLast xs)
+-- removeLast :: [String] -> [String]
+-- removeLast (x : y : []) = (x : [])
+-- removeLast (x : xs) = (x : removeLast xs)
 
-removeFirstAndLAst :: [String] -> [String]
-removeFirstAndLAst (x : xs) = removeLast xs
+-- removeFirstAndLAst :: [String] -> [String]
+-- removeFirstAndLAst (x : xs) = init xs
 
 getInsideParentheses :: [String] -> [String]
-getInsideParentheses str = removeFirstAndLAst (getParenthesis str 0)
-
+getInsideParentheses str = init $ tail (getParenthesis str 0)
