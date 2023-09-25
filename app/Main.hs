a32186ace62b088316a1a7da1f99c7ccefe60c64
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-09-25 15:10:27 +0200

    feat(Parser-Bootstrap): parse any char from bootstrap implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+import Data.Foldable (find)
+
 --
 -- EPITECH PROJECT, 2023
 -- Main.hs
@@ -5,7 +7,7 @@
 -- GLaDOS scraper Main file
 --
 
-data Position = Position { line :: Int, char :: Int }
+data Position = Position { line :: Int, char :: Int } deriving (Show)
 
 moveCursor :: Position -> Bool -> Position
 moveCursor current True = Position { line = line current + 1, char = char current }
@@ -19,3 +21,14 @@ parseChar char (x:xs) current
     | char == x = Right (x, xs, moveCursor current False)
     | otherwise = Left ( "Invalid char found", current )
 parseChar _ _ current = Left ( "Invalid char found", current )
+
+parseFoundChar :: Maybe Char -> Parser Char
+parseFoundChar Nothing _ current = Left ( "Char not found", current )
+parseFoundChar (Just '\n') string current = Right ('\n', string, moveCursor current True)
+parseFoundChar (Just a) string current =  Right (a, string, moveCursor current False)
+
+parseAnyChar :: String -> Parser Char
+parseAnyChar "" _ current = Left ( "Empty list of chars", current )
+parseAnyChar chars (x:xs) current = parseFoundChar (find (== x) chars) xs current
+parseAnyChar _ _ current = Left ( "List empty", current )
+
