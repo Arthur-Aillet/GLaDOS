d969ba08d5569c33765bd3b255697d99612d21de
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-09-25 14:50:23 +0200

    feat: exo 1.2.1

--- app/Main.hs
+++ app/Main.hs
@@ -1,49 +1,17 @@
-{-
+--
 -- EPITECH PROJECT, 2023
--- Main.hs
+-- Dev_repo
 -- File description:
--- GLaDOS scraper Main file
--}
+-- Main.hs
+ --
 
-module Main (main) where
+type Parser a = String -> Either String (a, String)
 
-import System.Environment (getArgs, getProgName)
-import System.IO (stdin, hGetContents', hIsTerminalDevice)
-import System.Timeout (timeout)
-import System.Exit
+parseChar :: Char -> Parser Char
+parseChar _ "" = Left ("No character")
+parseChar c (x:xs) | (x == c) = Right (c, xs)
+                   | otherwise = parseChar c xs
 
--- print the command line, with exec name and all args seperated by a space
--- note the lack of quoting for params containing a space
-cmd :: IO ()
-cmd = do
-    name <- getProgName
-    args <- getArgs
-    putStr name
-    putStr " "
-    mapM_ (\s -> putStr (' ':s)) args
-    putStrLn ""
-
--- print the stdin or fail if stdin is a tty
-cat :: IO ()
-cat = do
-    bool <- hIsTerminalDevice stdin
-    if bool
-        then
-            putStrLn "#ERR: input is tty"
-        else
-            do
-                contents <- hGetContents' stdin
-                putStrLn contents
-
--- dump input
-scraper :: IO ()
-scraper =  putStrLn "cmd:" >> cmd >> putStrLn "cat:" >> cat
-
--- wrap the scraper in a timeout loop to prevent apparent crash should
---  measures to avoid waiting on input to fail
-main :: IO ExitCode
+main :: IO ()
 main = do
-    status <- timeout (10 * 1000 * 1000) scraper
-    case status of
-        Just () -> exitSuccess
-        Nothing -> putStrLn "#ERR: timedout" >> exitWith (ExitFailure 84)
+    putStrLn $ "Exo 1.2.1: " ++ show (parseChar 'a' "abcd")
