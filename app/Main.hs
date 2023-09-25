e376e08db5c9fed5f16bc88644a8735dc3637e9a
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-09-25 15:09:26 +0200

    feat: exo 1.2.2

--- app/Main.hs
+++ app/Main.hs
@@ -8,10 +8,17 @@
 type Parser a = String -> Either String (a, String)
 
 parseChar :: Char -> Parser Char
-parseChar _ "" = Left ("No character")
+parseChar _ "" = Left ("No characters")
 parseChar c (x:xs) | (x == c) = Right (c, xs)
-                   | otherwise = parseChar c xs
+                   | otherwise = Left ("No characters")
+
+parseAnyChar :: String -> Parser Char
+parseAnyChar _ "" = Left ("No characters")
+parseAnyChar "" _ = Left ("No characters")
+parseAnyChar (x:xs) (y:ys) | (parseChar x (y:ys)) == Left "No characters" = parseAnyChar xs (y:ys)
+                        | otherwise = Right (x, ys)
 
 main :: IO ()
 main = do
     putStrLn $ "Exo 1.2.1: " ++ show (parseChar 'a' "abcd")
+    putStrLn $ "Exo 1.2.2: " ++ show (parseAnyChar "bca" "abcd")
