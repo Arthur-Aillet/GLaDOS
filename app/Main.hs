75a4ccc59734ef342164e40be72966addf73376a
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-09-25 16:17:30 +0200

    feat(Parser-Bootstrap): parse or from bootstrap implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 import Data.Foldable (find)
+import Data.Either (rights, lefts, isRight)
 
 --
 -- EPITECH PROJECT, 2023
@@ -9,11 +10,15 @@ import Data.Foldable (find)
 
 data Position = Position { line :: Int, char :: Int } deriving (Show)
 
+defaultPosition :: Position
+defaultPosition = Position { line = 0, char = 0 }
+
 moveCursor :: Position -> Bool -> Position
 moveCursor current True = Position { line = line current + 1, char = char current }
 moveCursor current False =  Position { line = line current, char = char current + 1 }
 
-type Parser a = String -> Position -> Either (String, Position) (a, String, Position)
+type ParserOutput a = Either (String, Position) (a, String, Position)
+type Parser a = String -> Position -> ParserOutput a
 
 parseChar :: Char -> Parser Char
 parseChar '\n' ('\n':xs) current = Right ('\n', xs, moveCursor current True)
@@ -32,3 +37,14 @@ parseAnyChar "" _ current = Left ( "Empty list of chars", current )
 parseAnyChar chars (x:xs) current = parseFoundChar (find (== x) chars) xs current
 parseAnyChar _ _ current = Left ( "List empty", current )
 
+validOrFirstError :: [ParserOutput a] -> Position -> ParserOutput a
+validOrFirstError [] current = Left ("Empty list", current)
+validOrFirstError list _
+    | null (rights list) = Left (head (lefts list))
+    | otherwise = Right (head (rights list))
+
+parseMultipleInclusif :: [Parser a] -> Parser a
+parseMultipleInclusif list string current = validOrFirstError (map (\x -> x string current) list) current
+
+parseOr :: Parser a -> Parser a -> Parser a
+parseOr first second = parseMultipleInclusif [first, second]
