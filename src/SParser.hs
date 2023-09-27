3d8f95a8a06b1519655a2f08dd45d37111a99642
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-09-27 22:36:19 +0200

    refactor(Parser): Coding style applied, guards replaced by pattern matching

--- src/SParser.hs
+++ src/SParser.hs
@@ -1,9 +1,9 @@
---
+{-
 -- EPITECH PROJECT, 2023
 -- GLaDOS
 -- File description:
 -- parser
---
+-}
 
 module SParser
   ( SExpr (SInt, SBool, SFloat, SSym, SList),
@@ -38,7 +38,10 @@ sExprParser str = strArrayToSExpr (sexprSplit str)
 
 strArrayToSExpr :: [String] -> [SExpr]
 strArrayToSExpr [] = []
-strArrayToSExpr ("(" : xs) = SList (strArrayToSExpr inParenthesis) : strArrayToSExpr withoutParenthesis
+strArrayToSExpr ("(" : xs) =
+  SList
+    (strArrayToSExpr inParenthesis)
+    : strArrayToSExpr withoutParenthesis
   where
     inParenthesis = getInsideParentheses ("(" : xs)
     withoutParenthesis = removeParenthesis ("(" : xs) 0
@@ -50,7 +53,10 @@ readBool "#t" = Just True
 readBool _ = Nothing
 
 readSExpr :: String -> SExpr
-readSExpr str = case (readMaybe str :: Maybe Int, readMaybe str :: Maybe Float, readBool str) of
+readSExpr str = case ( readMaybe str :: Maybe Int,
+                       readMaybe str :: Maybe Float,
+                       readBool str
+                     ) of
   (_, _, Just b) -> SBool b
   (Just i, _, _) -> SInt i
   (_, Just f, _) -> SFloat f
@@ -60,7 +66,10 @@ sexprSplit :: String -> [String]
 sexprSplit input = cleanedUp
   where
     cleanedUp = clean (clean splitIntput "") "\n"
-    splitIntput = separateCharOnList (separateCharOnList spaceSplitInput '(') ')'
+    splitIntput =
+      separateCharOnList
+        (separateCharOnList spaceSplitInput '(')
+        ')'
     spaceSplitInput = split ' ' input
 
 isBetween :: (Ord a) => a -> a -> a -> Bool
@@ -82,18 +91,16 @@ clean xs match = filter (match /=) xs
 getParenthesis :: [String] -> Int -> [String]
 getParenthesis [] _ = []
 getParenthesis (")" : _) 1 = [")"]
-getParenthesis (x : xs) i
-  | x == "(" = x : getParenthesis xs (i + 1)
-  | x == ")" = x : getParenthesis xs (i - 1)
-  | otherwise = x : getParenthesis xs i
+getParenthesis ("(" : xs) i = "(" : getParenthesis xs (i + 1)
+getParenthesis (")" : xs) i = ")" : getParenthesis xs (i + 1)
+getParenthesis (x : xs) i = x : getParenthesis xs i
 
 removeParenthesis :: [String] -> Int -> [String]
 removeParenthesis [] _ = []
 removeParenthesis (")" : xs) 1 = xs
-removeParenthesis (x : xs) i
-  | x == "(" = removeParenthesis xs (i + 1)
-  | x == ")" = removeParenthesis xs (i - 1)
-  | otherwise = removeParenthesis xs i
+removeParenthesis ("(" : xs) i = removeParenthesis xs (i + 1)
+removeParenthesis (")" : xs) i = removeParenthesis xs (i + 1)
+removeParenthesis (_ : xs) i = removeParenthesis xs i
 
 getInsideParentheses :: [String] -> [String]
 getInsideParentheses [] = []
