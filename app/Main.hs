81344175e0ac6e73a67d087e352d6204999acc11
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-09-27 02:05:09 +0200

    refactor(Parser): Parser () replaced with Parser $ for readebility

--- app/Main.hs
+++ app/Main.hs
@@ -23,28 +23,25 @@ newtype Parser a = Parser {
 }
 
 instance Functor Parser where
-    fmap fct parser = Parser (\string pos -> case runParser parser string pos of
+    fmap fct parser = Parser $ \string pos -> case runParser parser string pos of
         Right (a, new_string, new_pos) -> Right (fct a, new_string, new_pos)
         Left a -> Left a
-        )
 
 instance Applicative Parser where
-    pure a = Parser (\string pos -> Right (a, string, pos))
+    pure a = Parser $ \string pos -> Right (a, string, pos)
     (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-    (<*>) parserfct parsera = Parser (\string pos -> case runParser parserfct string pos of
+    (<*>) parserfct parsera = Parser $ \string pos -> case runParser parserfct string pos of
         Right (fct, new_string, new_pos) -> case runParser parsera new_string new_pos of
             Right (a, snd_string, snd_pos) -> Right (fct a, snd_string, snd_pos)
             Left a -> Left a
         Left a -> Left a
-        )
     (*>)  :: Parser a -> Parser b -> Parser b
-    a *> b = Parser (\string pos -> case runParser a string pos of
+    a *> b = Parser $ \string pos -> case runParser a string pos of
         Right (_, new_string, new_pos) -> runParser b new_string new_pos
         Left _ -> runParser b string pos
-        )
 
 instance Alternative Parser where
-    empty = Parser (\_ pos ->Left ("Empty", pos))
+    empty = Parser $ \_ pos ->Left ("Empty", pos)
     (<|>) :: Parser a -> Parser a -> Parser a
     first <|> second = Parser (\string pos -> case (runParser first string pos, runParser second string pos) of
         (Right (element, snd_string, snd_pos), Right _) -> Right (element, snd_string, snd_pos)
@@ -56,23 +53,21 @@ instance Alternative Parser where
 instance Monad Parser where
     (>>) = (*>)
     (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    a >>= fct =  Parser (\string pos -> case runParser a string pos of
+    a >>= fct =  Parser $ \string pos -> case runParser a string pos of
         Right (res, new_string, new_pos) -> case runParser (fct res) new_string new_pos of 
             Right (new, snd_string, snd_pos) -> Right (new, snd_string, snd_pos)
             Left err -> Left err
         Left err -> Left err
-        )
     return = pure
 
 
 withErr :: String -> Parser a -> Parser a
-withErr message parser = Parser (\string pos -> case runParser parser string pos of 
+withErr message parser = Parser $ \string pos -> case runParser parser string pos of 
     Right a -> Right a
     Left (_, new_pos) -> Left (message, new_pos)
-    )
 
 parseChar :: Char -> Parser Char
-parseChar char = Parser (\string pos -> case string of
+parseChar char = Parser $ \string pos -> case string of
     ('\n':xs)
         | '\n' == char -> Right ('\n', xs, moveCursor pos True)
         | otherwise -> Left ("Invalid char found", moveCursor pos True)
@@ -80,10 +75,9 @@ parseChar char = Parser (\string pos -> case string of
         | x == char -> Right (x, xs, moveCursor pos False)
         | otherwise -> Left ("Invalid char found", moveCursor pos False)
     _ -> Left ("Invalid char found", defaultPosition)
-    )
 
 parseAnyChar :: [Char] -> Parser Char
-parseAnyChar char = Parser (\string pos -> case string of
+parseAnyChar char = Parser $ \string pos -> case string of
     ('\n':xs)
         | '\n' `elem` char -> Right ('\n', xs, moveCursor pos True)
         | otherwise -> Left ("Invalid char found", moveCursor pos True)
@@ -91,7 +85,6 @@ parseAnyChar char = Parser (\string pos -> case string of
         | x `elem` char -> Right (x, xs, moveCursor pos False)
         | otherwise -> Left ("Invalid char found", moveCursor pos False)
     _ -> Left ("Invalid char found", defaultPosition)
-    )
 
 parseOr :: Parser a -> Parser a -> Parser a
 parseOr first second = first <|> second
@@ -103,12 +96,11 @@ parseAnd :: Parser a -> Parser b -> Parser (a, b)
 parseAnd = parseAndWith (,)
 
 parseMany :: Parser a -> Parser [a]
-parseMany parse = Parser (\string pos -> case runParser parse string pos of
+parseMany parse = Parser $ \string pos -> case runParser parse string pos of
     Right (element, new_string, new_pos) -> case runParser (parseMany parse) new_string new_pos of
         Left _ -> Right ([], new_string, new_pos)
         Right (found, found_string, found_pos) -> Right (element : found, found_string, found_pos)
     Left _ -> Right ([], string, pos)
-    )
 
 parseSome :: Parser a -> Parser [a]
 parseSome parse = (:) <$> parse <*> parseMany parse
