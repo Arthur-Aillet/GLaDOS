e0b37658c5ae409a929bc60732433b22f755a131
Arthur Aillet <58011909+Arthur-Aillet@users.noreply.github.com>
2023-09-28 00:55:30 +0200

    Revert "fix: Cleaning repository before first release"

--- app/Converter.hs
+++ app/Converter.hs
@@ -1,50 +1,51 @@
-{-
+--
 -- EPITECH PROJECT, 2023
 -- glados
 -- File description:
 -- Converter
--}
+--
 
 module Converter (sexprToAST) where
 
-import AST (Ast (Atom, Call, Define, If, Lambda, Symbol))
-import SParser (SExpr (SInt, SList, SSym))
+import AST (Ast (Symbol, Atom, Call, Define, Lambda, If))
+import SParser (SExpr (SInt, SSym, SList))
 
 convertArgsContinuous :: [SExpr] -> Maybe [Ast]
-convertArgsContinuous (x : xs) = case convertArgsContinuous xs of
-  (Just ys) -> case sexprToAST x of
-    Just y -> Just (y : ys)
+convertArgsContinuous (x:xs) = case convertArgsContinuous xs of
+    (Just ys) -> case sexprToAST x of
+        Just y -> Just (y:ys)
+        Nothing -> Nothing
     Nothing -> Nothing
-  Nothing -> Nothing
 convertArgsContinuous [] = Just []
 
 convertSymbols :: [SExpr] -> Maybe [String]
-convertSymbols (SSym sym : xs) = case convertSymbols xs of
-  (Just ys) -> Just (sym : ys)
-  Nothing -> Nothing
+convertSymbols (SSym sym:xs) = case convertSymbols xs of
+    (Just ys) -> Just (sym:ys)
+    Nothing -> Nothing
 convertSymbols [] = Just []
 convertSymbols _ = Nothing
 
 sexprToAST :: SExpr -> Maybe Ast
 sexprToAST (SList [x]) = sexprToAST x
-sexprToAST (SList [SSym "define", SSym name, s]) = case mexpr of
-  Just expr -> Just $ Define name expr
-  Nothing -> Nothing
-  where
-    mexpr = sexprToAST s
-sexprToAST (SList [SSym "define", SList (SSym name : args), expr]) =
-  case convertSymbols args of
+sexprToAST (SList [SSym "define",SSym name,s]) = case mexpr of
+        Just expr -> Just $ Define name expr
+        Nothing -> Nothing
+    where mexpr = sexprToAST s
+sexprToAST (SList [SSym "define", SList (SSym name:args), expr]) =
+    case convertSymbols args of
     (Just jArgs) -> case sexprToAST expr of
-      (Just jExpr) -> Just (Define name (Lambda jArgs jExpr))
-      Nothing -> Nothing
+        (Just jExpr) -> Just (Define name (Lambda jArgs jExpr))
+        Nothing -> Nothing
     _ -> Nothing
+
 sexprToAST (SList [SSym "if", _if, _then, _else]) =
-  case (sexprToAST _if, sexprToAST _then, sexprToAST _else) of
-    (Just jIf, Just jThen, Just jElse) -> Just (If jIf jThen jElse)
+    case (sexprToAST _if, sexprToAST _then, sexprToAST _else) of
+        (Just jIf, Just jThen, Just jElse) -> Just (If jIf jThen jElse)
+        _ -> Nothing
+
+sexprToAST (SList (SSym name:args)) = case convertArgsContinuous args of
+    (Just jArgs) -> Just (Call (Symbol name) jArgs)
     _ -> Nothing
-sexprToAST (SList (SSym name : args)) = case convertArgsContinuous args of
-  (Just jArgs) -> Just (Call (Symbol name) jArgs)
-  _ -> Nothing
 sexprToAST (SInt x) = Just $ Atom x
 sexprToAST (SSym x) = Just $ Symbol x
 sexprToAST (SList _) = Nothing
