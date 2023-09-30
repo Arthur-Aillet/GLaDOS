b81edc3f809bf64a2f69afa3c48e870377ec0efd
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-09-30 10:59:22 +0200

    feat: parseFloat :: Parser Float

--- src/ParserInt.hs
+++ src/ParserInt.hs
@@ -10,13 +10,23 @@ module ParserInt (module ParserInt) where
 import Control.Applicative (Alternative ((<|>)))
 import ParserChar (parseChar, parseDigit)
 import ParserType (Parser (..))
-import SyntaxParser (parseSome)
+import SyntaxParser (parseSome, parseMany)
+
+parseStringInt :: Parser String
+parseStringInt = parseSome parseDigit
 
 parseUInt :: Parser Int
-parseUInt = read <$> parseSome parseDigit
+parseUInt = read <$> parseStringInt
 
 parseNegInt :: Parser Int
 parseNegInt = (* (-1)) <$> (parseChar '-' *> parseUInt)
 
 parseInt :: Parser Int
 parseInt = parseNegInt <|> parseUInt
+
+parseFloat :: Parser Float
+parseFloat = (\intPart charPart -> read (intPart ++ charPart))
+            <$> parseStringInt <*> parseFractionalPart
+
+parseFractionalPart :: Parser String
+parseFractionalPart = ('.' :) <$> (parseChar '.' *> parseMany parseDigit)
