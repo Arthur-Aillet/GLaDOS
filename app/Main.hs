888dfda270b5973b203cfdc2801b9b16543e0da5
Auguste Frater <augustefrater@gmail.com>
2023-10-02 12:22:39 +0200

    feat: added prompt if file not provided

--- app/Main.hs
+++ app/Main.hs
@@ -8,20 +8,63 @@
 module Main (main) where
 
 import AST (Context, emptyContext, evalAST)
+import System.Environment (getArgs, getProgName)
 import Converter (sexprToAST)
+import System.Timeout (timeout)
 import SParser (SExpr, sExprParser)
 import System.Exit
-import System.IO (hGetContents', stdin)
+import System.IO (hGetContents', stdin, hIsTerminalDevice, hSetBuffering, stdout, BufferMode(..))
 
-testInput :: IO [SExpr]
-testInput = do
-  contents <- hGetContents' stdin
-  return $ sExprParser contents
+-- print the command line, with exec name and all args seperated by a space
+-- note the lack of quoting for params containing a space
+cmd :: IO ()
+cmd = do
+    name <- getProgName
+    args <- getArgs
+    putStr name
+    putStr " "
+    mapM_ (\s -> putStr (' ':s)) args
+    putStrLn ""
 
+-- print the stdin or fail if stdin is a tty
+cat :: IO ()
+cat = do
+    bool <- hIsTerminalDevice stdin
+    if bool
+        then
+            putStrLn "#ERR: input is tty"
+        else
+            do
+                contents <- hGetContents' stdin
+                _ <- loopOnCommands emptyContext (sExprParser contents)
+                pure()
+
+-- dump input
+scraper :: IO ()
+scraper =  putStrLn "cmd:" >> cmd >> putStrLn "cat:" >> cat
+
+getInstructions :: IO ()
+getInstructions = do
+                    putStr "glados >"
+                    line <- getLine
+                    _ <- loopOnCommands emptyContext (sExprParser line)
+                    getInstructions
+
+-- wrap the scraper in a timeout loop to prevent apparent crash should
+--  measures to avoid waiting on input to fail
 main :: IO ExitCode
 main = do
-  expr <- testInput
-  loopOnCommands emptyContext expr
+    hSetBuffering stdout NoBuffering
+    bool <- hIsTerminalDevice stdin
+    if bool
+        then do
+            getInstructions
+            exitSuccess
+    else do
+        status <- timeout (10 * 1000 * 1000) scraper
+        case status of
+            Just () -> exitSuccess
+            Nothing -> putStrLn "#ERR: timedout" >> exitWith (ExitFailure 84)
 
 loopOnCommands :: Context -> [SExpr] -> IO ExitCode
 loopOnCommands _ [] = exitSuccess
