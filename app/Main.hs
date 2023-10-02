508dbee08c473e5c1e005d2cb622c0d57294cfb4
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-10-02 18:37:32 +0200

    fix(Parser): parseMany2 refactored to have wished functionality of parseManyValidOrEmpty

--- app/Main.hs
+++ app/Main.hs
@@ -15,12 +15,12 @@ import System.IO (hGetContents', stdin)
 import ParserSExpr
 import PositionType
 import ParserType (Parser(..))
-import SyntaxParser (parseMany)
+import SyntaxParser (parseManyValidOrEmpty)
 
 main :: IO ExitCode
 main = do
   contents <- hGetContents' stdin
-  case runParser (parseMany parseSExpr) contents defaultPosition of
+  case runParser (parseManyValidOrEmpty parseSExpr) contents defaultPosition of
     Left (err, pos) -> putStrLn (show err ++ " found at: " ++ show pos) >> exitWith (ExitFailure 84)
     Right (sexpr, _, _) -> print sexpr >> loopOnCommands emptyContext sexpr
 
--- src/SyntaxParser.hs
+++ src/SyntaxParser.hs
@@ -18,20 +18,35 @@ parseMany parse = Parser $ \string pos -> case runParser parse string pos of
       Right (found, fd_str, fd_pos) -> Right (element : found, fd_str, fd_pos)
   Left _ -> Right ([], string, pos)
 
-parseMany2 :: Parser a -> Parser [a]
-parseMany2 parse = Parser $ \string pos -> case runParser parse string pos of
+parseSpaces :: Parser Char
+parseSpaces = parseAnyChar [' ', '\n', '\t']
+
+parseOnlySpaces :: Parser String
+parseOnlySpaces = Parser $ \string pos -> case string of
+  [] -> Right ([], string, pos)
+  _ -> case runParser parseSpaces string pos of
+    Right (new, new_str, new_pos) ->
+      case runParser parseOnlySpaces new_str new_pos of
+        Left a -> Left a
+        Right (found, fd_str, fd_pos) -> Right (new : found, fd_str, fd_pos)
+    Left a -> Left a
+
+parseManyValidOrEmpty :: Parser a -> Parser [a]
+parseManyValidOrEmpty parse = Parser $ \string pos -> case runParser parse string pos of
   Right (element, new_str, new_pos) ->
-    case runParser (parseMany parse) new_str new_pos of
-      Left _ -> Left ("Error", new_pos)
+    case runParser (parseManyValidOrEmpty parse) new_str new_pos of
+      Left a -> case runParser parseOnlySpaces new_str new_pos of
+        Left _ -> Left a
+        Right (_, fd_str, fd_pos) ->  Right ([element], fd_str, fd_pos)
       Right (found, fd_str, fd_pos) -> Right (element : found, fd_str, fd_pos)
-  Left _ -> Left ("Error", pos)
+  Left a -> Left a
 
 parseSome :: Parser a -> Parser [a]
 parseSome parse = (:) <$> parse <*> parseMany parse
 
 parseWithSpace :: Parser a -> Parser a
 parseWithSpace parser =
-  parseMany (parseAnyChar [' ', '\n']) *> parser <* parseMany (parseAnyChar [' ', '\n'])
+  parseMany parseSpaces *> parser <* parseMany parseSpaces
 
 parsePair :: Parser a -> Parser (a, a)
 parsePair parser =
