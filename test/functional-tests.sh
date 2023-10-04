77d4236b316ea63623fd4e5bb48a4985318f72d5
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-10-04 16:02:11 +0200

    feat(CI): now functional tests could run or not unsupported tests cases and is added to the CI Workflow

--- test/functional-tests.sh
+++ test/functional-tests.sh
@@ -2,13 +2,16 @@
 
 usage=0
 silent=0
+unsup=0
 
-while getopts 'sh' opt; do
+while getopts 'shu' opt; do
   case "$opt" in
-    h) echo -e "USAGE\n -h: get usage\n -s: disable error explanation"
+    h) echo -e "USAGE\n -h: get usage\n -s: disable error explanation\n -u: unsupported samples even tested"
     usage=1
       ;;
     s) silent=1
+      ;;
+    u) unsup=1
       ;;
     ?) usage=1
   esac
@@ -62,6 +65,34 @@ then
       fi
     fi
   done
+
+  if [ ${unsup} == 1 ]
+  then
+    echo
+    echo "[ Usupported samples testing ]"
+    echo
+    FILES="./test/unsupported_samples/*"
+    for f in $FILES
+    do
+      ((nbr=nbr+1))
+      NAME=${f##*/}
+      printf "Testing [%-15s] file:" $NAME
+      ./glados < $f > /tmp/GLaDOS_Tests/current_glados
+      scheme -q < $f > /tmp/GLaDOS_Tests/current_scheme
+      DIFF=$(diff /tmp/GLaDOS_Tests/current_glados /tmp/GLaDOS_Tests/current_scheme) 
+      if [ "$DIFF" == "" ] 
+      then
+        ((nbr_passed=nbr_passed+1))
+        echo -e "$2\t\t${Green}PASSED${NoColor}"
+      else
+        echo -e "$2\t\t${Red}NOT PASSED${NoColor}"
+        if [ $silent == 0 ]
+          then
+          diff -y /tmp/GLaDOS_Tests/current_glados /tmp/GLaDOS_Tests/current_scheme
+        fi
+      fi
+    done
+  fi
 
   echo
   echo  Tests passed [${nbr_passed} / ${nbr}]
