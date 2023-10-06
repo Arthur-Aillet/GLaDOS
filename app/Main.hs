282e2c305aefc58ec1215e9b124bfb4cdf02d960
Auguste Frater <augustefrater@gmail.com>
2023-10-06 23:44:03 +0200

    feat: replaced getline with haskeline librairy. autocompletion done with keywords but not symbols

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,24 @@ import PositionType
 import SyntaxParser (parseManyValidOrEmpty)
 import System.Exit
 import System.IO (BufferMode (..), hGetContents', hIsTerminalDevice, hSetBuffering, stdin, stdout)
+import Data.List
+import System.Console.Haskeline
+import Control.Monad.IO.Class
+
 import System.Timeout (timeout)
+
+--would be sweet if i ever find how to expand this based on the context
+keywords = [ "(define", "(lambda", "(eq?", "(div", "(mod", "(if"]
+
+search :: String -> [Completion]
+search str = map simpleCompletion $ filter (str `isPrefixOf`) keywords
+
+customSettings :: MonadIO m => Settings m
+customSettings = Settings {
+                  complete = completeWord Nothing " \t" $ return . search,
+                  historyFile = Nothing,
+                  autoAddHistory = True
+                }
 
 executeFile :: IO ()
 executeFile = do
@@ -27,22 +44,26 @@ executeFile = do
       _ <- loopOnCommands emptyContext sexpr
       exitSuccess
 
-getInstructions :: Context -> IO ExitCode
+getInstructions :: Context -> InputT IO ()
 getInstructions context = do
-  putStr "GLaDOS> "
-  new_line <- getLine
-  case runParser (parseManyValidOrEmpty parseSExpr) new_line defaultPosition of
-    Left err -> printErr err >> exitWith (ExitFailure 84)
-    Right (sexpr, _, _) -> do
-      new_context <- loopOnCommands context sexpr
-      getInstructions new_context
+  new_line <- getInputLine "\ESC[31m\STXG\ESC[0;33m\STXL\ESC[3;32m\STXa\ESC[0;32m\STXD\ESC[34m\STXO\ESC[35m\STXS\ESC[0m\STX>:"
+  case new_line of
+    Nothing -> getInstructions context
+    Just "quit" -> liftIO $ exitWith $ ExitSuccess
+    Just "" -> getInstructions context
+    Just str ->
+      case runParser (parseManyValidOrEmpty parseSExpr) str defaultPosition of
+        Left err -> liftIO (printErr err) >> liftIO (exitWith (ExitFailure 84))
+        Right (sexpr, _, _) -> do
+          new_context <- liftIO (loopOnCommands context sexpr)
+          getInstructions new_context
 
-main :: IO ExitCode
+main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
   bool <- hIsTerminalDevice stdin
   if bool
-    then getInstructions emptyContext >> exitSuccess
+    then runInputT customSettings (getInstructions emptyContext) >> exitSuccess
     else do
       status <- timeout 10000000 executeFile
       case status of
